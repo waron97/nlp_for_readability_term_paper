7f15c50782161ae330699931f7056344d8df6cb3
waron97 <winkler.aron5gmail.com>
2023-03-31 16:27:11 +0200

    update dataset writer

--- write_dataset_csv.py
+++ write_dataset_csv.py
@@ -2,6 +2,7 @@ import csv
 from src.sql import engine
 from src.sql.models import ClassifierDataset
 from sqlalchemy.orm.session import Session
+import random
 
 
 def main():
@@ -11,12 +12,23 @@ def main():
     test_csv_path = "data/dataset_test.csv"
     with Session(engine) as session:
         q = session.query(ClassifierDataset)
+        train_rows_standard = []
+        train_rows_simple = []
+
         for item in q:
             row = [item.id, item.level, item.text]
             if item.partition == "train":
-                train_rows.append(row)
+                if item.level == "simple":
+                    train_rows_simple.append(row)
+                elif item.level == "standard":
+                    train_rows_standard.append(row)
             else:
                 test_rows.append(row)
+
+        random.shuffle(train_rows_standard)
+        train_rows.extend(train_rows_simple)
+        train_rows.extend(train_rows_standard[:len(train_rows_simple)])
+
     with open(train_csv_path, "w", encoding="utf-8", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(train_rows)
